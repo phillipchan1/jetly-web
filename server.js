f5c888f957049384a31295c048cc18a6a8ac9070
Phillip_Chan <phillip.chan@liferay.COM>
2018-01-29 14:22:13 -0800

    Add prod build

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.all('/*', function(req, res) {
+  res
+    .status(200)
+    .set({ 'content-type': 'text/html; charset=utf-8' })
+    .sendFile(process.cwd() + '/dist/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
