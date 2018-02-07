4e66bd9381a617e4cfa7307acd0bd918fecb8b3a
Phillip_Chan <phillip.chan@liferay.COM>
2018-02-07 09:58:19 -0800

    SF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,33 +5,33 @@ import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 
 @Component({
-	selector: 'app-root',
-	host: { '(window:keydown)': 'hotkeys($event)' },
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  host: { '(window:keydown)': 'hotkeys($event)' },
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	loggedIn: boolean = false;
+  loggedIn: boolean = false;
 
-	constructor(private router: Router, public afAuth: AngularFireAuth) {
-		this.afAuth.authState.subscribe(res => {
-			if (!res || !res.uid) {
-				this.router.navigate(['/login']);
-			} else {
-				this.loggedIn = true;
-			}
-		});
-	}
+  constructor(private router: Router, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(res => {
+      if (!res || !res.uid) {
+        this.router.navigate(['/login']);
+      } else {
+        this.loggedIn = true;
+      }
+    });
+  }
 
-	hotkeys(event) {
-		// if (event.key === 'a') {
-		// 	this.router.navigate(['/review']);
-		// }
-		// if (event.key === 's') {
-		// 	this.router.navigate(['/board']);
-		// }
-		// if (event.key === 'd') {
-		// 	this.router.navigate(['/plan']);
-		// }
-	}
+  hotkeys(event) {
+    // if (event.key === 'a') {
+    // 	this.router.navigate(['/review']);
+    // }
+    // if (event.key === 's') {
+    // 	this.router.navigate(['/board']);
+    // }
+    // if (event.key === 'd') {
+    // 	this.router.navigate(['/plan']);
+    // }
+  }
 }
