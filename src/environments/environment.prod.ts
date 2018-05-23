1f5f134c0804ba1834cc171c3561afe616980557
Phillip Chan <phillip.chan@liferay.com>
2018-05-23 12:08:37 -0700

    Fix prod API key bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	firebase: {
-		apiKey: 'AIzaSyC1M3Kr7cXwY7MXfXtK0DyGjuzgr2u89Og ',
+		apiKey: 'AIzaSyC1M3Kr7cXwY7MXfXtK0DyGjuzgr2u89Og',
 		authDomain: 'jetly-186ed.firebaseapp.com',
 		databaseURL: 'https://jetly-186ed.firebaseio.com',
 		projectId: 'jetly-186ed',
